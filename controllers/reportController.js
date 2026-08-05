0af2705f4794b05032c2e83f43ba451f81c55b13
rkataria99 <katariarutvika@gmail.com>
2026-08-05 15:40:34 +0530

    Mom Fb subcategory added in Direct home share/family

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -15,7 +15,7 @@ const EXPENSE_GROUPS = [
 ];
 
 const SUBCATEGORIES_BY_GROUP = {
-  home_share: ["Direct home share", "Grocery", "Family Exp", "Misc"],
+  home_share: ["Direct home share", "Grocery", "Family Exp", "Mom Fb", "Misc"],
   self: ["Food", "Movies", "Party", "Transport", "Outings", "Other"],
   gifts_family: ["Gifts", "Family dinner", "Family outing"],
   trip_family: ["Travel", "Stay", "Food", "Shopping", "Entire Trip Cost", "Misc"],
